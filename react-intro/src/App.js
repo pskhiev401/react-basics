954b76e106f6f4058a4392f05b5f468a2d62bec6
Patric Khiev <pskhiev@gmail.com>
2018-09-08 12:02:25 -0500

    simple react example

---
+++ react-intro/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      name: 'bob',
+      age: null,
+      display: 'test'
+    }
+  }
+
+  changeName = (event) => {
+    this.setState({name:event.target.value})
+  }
+
+  clickHandler = (name) => {
+    this.setState({display:name});   
+  }
+  
+  render() {
+    console.log(this.state)
+    return (
+      <div>
+        <p>Temp: {this.state.name} </p>
+        <p>Name: {this.state.display}</p>
+        <input onChange= {(event) => this.changeName (event)} placeholder='Enter Name Here'/>
+        <button onClick= {() => this.clickHandler(this.state.name)} >Submit Here</button>
+        <p>Age: {this.state.age}</p>
+        <input placeholder='Enter Age Here'/>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
